661daca6e8b1c13e8704f05eac0ea9a6c6d9f41b
Jacob Potter <jacob@durbatuluk.us>
2012-01-01 19:31:52 -0500

    Understand some more instructions.

--- arch/arm.py
+++ arch/arm.py
@@ -10,11 +10,11 @@ safe_insn_bases = [
 	"clz", "cmn", "cmp", "cps", "cpsid", "cpsie", "cpy",
 	"eor", "eors",
 	"it", "ite", "itet", "itete", "itett", "itt", "itte", "ittet",
-		"ittt", "itttt",
-	"lsl", "lsls", "lsr", "lsrs",
+		"ittt", "itttt", "itee", "iteee", "ittte",
+	"lsl", "lsls", "lsr", "lsrs", "sbfx",
 	"ldrex", "ldrexb", "ldrexh",
 	"mla", "mls", "mov", "movs", "movt", "movw", "mrs", "msr", "mul",
-		"muls", "mvn", "mvns",
+		"muls", "mvn", "mvns", "smull",
 	"neg", "negs", "nop",
 	"orn", "orr", "orrs",
 	"rbit", "rev", "rev16", "revsh", "ror", "rsb", "rsbs",
@@ -25,7 +25,7 @@ safe_insn_bases = [
 	"wfe", "wfi",
 
 	# NOTE: these are only safe because we translate ldmia and stmdb on sp to push and pop.
-	"ldmia", "stmdb", "stmia",
+	"ldmia", "stmdb", "stmia", "ldmdb",
 
 #	"ldr", "ldrb", "ldrd", "ldrh", "ldrsb", "ldrsh", "str", "strb", "strd", "strh",
 ]
@@ -108,6 +108,11 @@ def canonicalize_line(function_name, line):
 		if pargs[-1] == 'pc':
 			ret.append(("ret", True))
 		return ret
+	elif insn == "stmia.w":
+		ret = [ ("stack", -4 * (len(pargs) - 1)) ]
+		if pargs[-1] == 'pc':
+			ret.append(("ret", True))
+		return ret
 	elif insn == "add" and operand:
 		return "stack", -operand
 	elif insn == "sub" and operand:
@@ -130,7 +135,7 @@ def canonicalize_line(function_name, line):
 		return "ret", True
 	elif insn.startswith("bx") and args == "lr":
 		return "ret", False
-	elif insn in [ "tbb", "tbh" ]:
+	elif insn in [ "tbb", "tbh" ] or (insn == "ldr.w" and pargs[0] == "pc"):
 		# We're pretty sure that table jumps are OK, but let the
 		# next stage know about them.
 		return "tablejump", None
